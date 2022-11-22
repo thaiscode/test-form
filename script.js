307f9023263486ae1b4d303828547b7bce4c5f4e
Thais Silva <83255298+thaiscode@users.noreply.github.com>
2022-11-22 17:29:29 -0300

    update

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 let form = document.querySelector('.form')
 let button = document.querySelector('.button')
-
+let inputs = document.querySelector('.input')
+let div = document.querySelector('.div')
 button.addEventListener('click', onSubmit)
 
 function onSubmit() {
+    function validate() {
+        for (i = 0; i = 1; i++) {
+            if (inputs[i] == "") {
+                div.innerHTML = "tá vazio"
+            }
+        }
+    }
+
     if (form.checkValidity() == false) {
-    console.log('falta coisa')
-} else if (form.checkValidity() == true)
-console.log('deu certo')
+        console.log('falta coisa')
+    } else if (form.checkValidity() == true) {
+        console.log('deu certo')
+    }
 }
