0184c901e732c4580af8cce086da081bf68dc302
Thais Silva <83255298+thaiscode@users.noreply.github.com>
2022-11-22 17:01:06 -0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let form = document.querySelector('.form')
 let button = document.querySelector('.button')
 
+button.addEventListener('click', onSubmit)
 
-if (form.checkValidity() == false) {
+function onSubmit() {
+    if (form.checkValidity() == false) {
     console.log('falta coisa')
+}
 }
